6fa2b9501d2c750ec47cc5f54e01b2815192a8ca
Anna Nonaka <annanonaka@gmail.com>
2021-08-05 22:16:39 +0900

    done creating model, controller and routes

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,7 +1,7 @@
 class Ingredient < ApplicationRecord
-  belong_to :order
+  belongs_to :order
   has_many :categories, through: :categories_ingredients
-  has_one_attached :image
+  has_attached :image
 
   validates :item_name, :item_quantity, :item_life, :price, :supplier, :unit, :weight, :expiry_date, :par_level, :minimum_amount, presence: true
 end
