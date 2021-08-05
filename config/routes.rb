3841d7d986c20fb4c8a5ac7398403264e4186cf7
Yui Kondo <yuikondobook@gmail.com>
2021-08-05 21:40:18 +0900

    done for the routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
    get "restaurants_settings", to: "restaurants#edit", as: :restaurant_settings
+  resources :orders, only: [:index, :create, :update]
+  
 end
