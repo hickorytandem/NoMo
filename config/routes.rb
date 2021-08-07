8cfd7f5e1ed66a7ec02c45ecf729a2dd9c1d7e1e
Namkhing Lee <nonlapat.lee@gmail.com>
2021-08-07 10:42:03 +0700

    Create category-ingredient controller + fix ingredient routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
    get "restaurants_settings", to: "restaurants#edit", as: :restaurant_settings
-   get "ingredients", to: "ingredients#index"
-   get "ingredients/new", to: "ingredients#new"
-   post "ingredients", to: "ingredients#create"
-   get "ingredients/:id", to: "ingredients#show"
-   patch "ingredients/:id", to: "ingredients#update"
-   delete "ingredients/:id", to: "ingredients#destroy"
+   resources :ingredients
+  #  get "ingredients", to: "ingredients#index"
+  #  get "ingredients/new", to: "ingredients#new"
+  #  post "ingredients", to: "ingredients#create"
+  #  get "ingredients/:id", to: "ingredients#show"
+  #  patch "ingredients/:id", to: "ingredients#update"
+  #  delete "ingredients/:id", to: "ingredients#destroy"
 end
